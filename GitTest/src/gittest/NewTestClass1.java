0fc76917321bb06832cfe3e01f717629bee4a334
matheg <matheg29@gmail.com>
2016-09-13 16:07:22 +0200

    Test
    //Kommentar- Hvordan fungerer dette?

--- GitTest/src/gittest/NewTestClass1.java
+++ GitTest/src/gittest/NewTestClass1.java
@@ -1,3 +1,4 @@
+    // TESt test testbranch
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -21,6 +22,7 @@ public class NewTestClass1 {
 =======
     //christerstuff
 }
+//Hvordan fungerer git? -Matias
 
 //kjkjjkj
 //hkhfowejdwqkpdqwædkq
